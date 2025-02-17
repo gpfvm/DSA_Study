3ffb5bff9e4d126b4bde22df20a5c8767b43e9df
Sung Seunghyun <70056397+moondancer92@users.noreply.github.com>
2025-02-17 18:16:34 +0900

    done

--- Ex0107_BinarySearch.cpp
+++ Ex0107_BinarySearch.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>      // std::setw
 
 #include <cassert>
@@ -29,27 +29,27 @@ int BinarySearch(int* arr, int n, int x) // 이진 탐색
 	{
 		PrintHelper(arr, n, left, right);
 
-		// int middle = ... ; // 정수 나누기 (버림)
+        int middle = (left + right) / 2 ; // 정수 나누기 (버림)
 
-		// cout << "middle " << middle << endl;
+		cout << "middle " << middle << endl;
 
-		//if ()
-		//{
-		//	...
-		//	cout << "right " << right << endl;
-		//}
-		//else if ()
-		//{
-		//	...
-		//	cout << "left " << left << endl;
-		//}
-		//else
-		//{
-		//	cout << "Found " << middle << endl;
-		//	return middle;
-		//}
+		if (arr[middle] > x)
+		{
+            right = middle -1;
+            cout << "right " << right << endl;
+		}
+		else if (arr[middle] < x)
+		{
+            left = middle + 1;
+            cout << "left " << left << endl;
+		}
+		else
+		{
+            cout << "Found " << middle << endl;
+            return middle;
+		}
 
-		break; // 임시: 무한루프 방지
+		//break; // 임시: 무한루프 방지
 	}
 
 	cout << "Not found" << endl;
@@ -70,7 +70,7 @@ int main()
 	BinarySearch(arr, n, 10);
 
 	//for (int x = 0; x < n; x++)
-	//	cout << x << " " << BinarySearch(arr, n, x) << endl;
+        //cout << x << " " << BinarySearch(arr, n, x) << endl;
 
 	return 0;
 }
