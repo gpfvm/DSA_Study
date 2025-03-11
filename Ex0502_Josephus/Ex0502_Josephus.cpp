08084b66537ab6332d32483d6925f281aa2b915d
Sung Seunghyun <70056397+moondancer92@users.noreply.github.com>
2025-03-11 13:34:37 +0900

    sovled

--- Ex0502_Josephus/Ex0502_Josephus.cpp
+++ Ex0502_Josephus/Ex0502_Josephus.cpp
@@ -1,4 +1,4 @@
-﻿#include "../shared/Queue.h"
+#include "../shared/Queue.h"
 
 #include <iostream>
 
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	int n = 7, k = 3; // set n and k to the desired values
+	int n = 4, k = 3; // set n and k to the desired values
 
 	Queue<int> q(n + 1);
 	q.SetDebugFlag(false);
@@ -17,15 +17,30 @@ int main()
 	q.Print();
 
 	// 마지막 한 명이 남을 때까지 반복
-	{
-		// k-1 명은 맨 앞에서 뒤로 보내기
+    {   while(q.Size() !=1)
+    {
+            // k-1 명은 맨 앞에서 뒤로 보내기
+            for(int j = k; 1<= j-1; j--)
+            {
+                int front = q.Front();
+                q.Enqueue(front);
+                q.Dequeue();
+                
+            }
+            
+            cout << "Before Executed " << endl;
+            q.Print();
+            // k 번째 사람 처형
+            cout << "Executed " << q.Front() << endl;
+            q.Dequeue();
+            cout << "after Executed " << endl;
+            q.Print();
+        }
 
-		// k 번째 사람 처형
-		// cout << "Executed " << ... << endl;
 	}
 
 	// 최후의 생존자 번호
-	// cout << "Survivor: " << ... << endl;
+    cout << "Survivor: " << q.Front() << endl;
 
 	return 0;
 }
