ea892728766bacf79edf4dafb4d74001dece5c6d
Sung Seunghyun <70056397+moondancer92@users.noreply.github.com>
2025-03-18 23:55:37 +0900

    Sovled

--- shared/DoublyLinkedList.h
+++ shared/DoublyLinkedList.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include <cassert>
 #include <stdint.h>
@@ -30,11 +30,13 @@ public:
 	void Clear() // 모두 지워야(delete) 합니다.
 	{
 		// TODO:
+        while(first_)
+            PopFront();
 	}
 
 	bool IsEmpty()
 	{
-		return true; // TODO:
+		return first_ == nullptr; // TODO:
 	}
 
 	int Size()
@@ -42,6 +44,11 @@ public:
 		int size = 0;
 
 		// TODO:
+        Node* current = first_;
+        while (current) {
+            current = current->right;
+            size += 1;
+        }
 
 		return size;
 	}
@@ -60,17 +67,45 @@ public:
 
 			cout << " Forward: ";
 			// TODO:
-			cout << endl;
+            
+            while(true)
+            {
+                cout << current->item;
+                
+                if(!current->right)
+                    break;
+                current = current->right;
+            }
+            cout << endl;
 
 			cout << "Backward: ";
+            
 			// TODO:
+            while(true)
+            {
+                cout << current->item;
+                
+                if(!current->left)
+                    break;
+                current = current->left;
+            }
 			cout << endl;
 		}
 	}
 
 	Node* Find(T item)
 	{
-		return nullptr; // TODO:
+		// TODO:
+        
+        Node* current = first_;
+        while (current)
+        {
+            if(current->item == item)
+                return current;
+            
+            current = current->right;
+        }
+        return current;
 	}
 
 	void InsertBack(Node* node, T item)
@@ -82,17 +117,49 @@ public:
 		else
 		{
 			// TODO:
+            Node* new_node = new Node;
+            new_node->item = item;
+            
+            Node* temp = nullptr;
+            temp = node->right;
+            node->right = new_node;
+            new_node->left = node;
+            new_node->right = temp;
+            temp->left = new_node;
 		}
 	}
 
 	void PushFront(T item)
 	{
 		// TODO:
+        Node* new_node = new Node;
+        new_node->item = item;
+        
+        new_node->right = first_;
+        if(first_)
+            first_->left = new_node;
+        first_ = new_node;
+        new_node->left = nullptr;
+
 	}
 
 	void PushBack(T item)
 	{
 		// TODO:
+        if(first_)
+        {
+            Node* current = first_;
+            while (current->right)
+                current = current->right;
+            
+            Node* new_node = new Node;
+            new_node->item = item;
+            new_node->left = current;
+            current->right = new_node;
+        }
+        else
+            PushFront(item);
+        
 	}
 
 	void PopFront()
@@ -107,6 +174,11 @@ public:
 		assert(first_);
 
 		// TODO:
+        Node* temp = first_;
+        first_ = first_->right;
+        if (first_)
+            first_->left = nullptr;
+        delete temp;
 	}
 
 	void PopBack()
@@ -123,25 +195,67 @@ public:
 		assert(first_);
 
 		// TODO:
+        using namespace std;
+        Node* current = first_;
+        
+        if(current->right)
+        {
+            while (current->right->right)
+                current = current->right;
+            
+            Node* temp = current->right;
+            current->right = nullptr;
+            delete temp;
+        }
+        else
+        {
+            delete first_;
+            first_ = nullptr;
+        }
+        
+       
 	}
 
 	void Reverse()
-	{
-		// TODO:
+    {
+        // TODO:
+        if(IsEmpty())
+            return;
+        else
+        {
+            Node* prev = nullptr;
+            Node* current = first_;
+            
+            while(current)
+            {
+                prev = current;
+                
+                current = current->right;
+                
+                std::swap(prev->left,prev->right);
+            }
+            first_ = prev;
+        }
 	}
 
 	T Front()
 	{
 		assert(first_);
 
-		return T(); // TODO:
+		//TODO:
+        return first_->item;
+
 	}
 
 	T Back()
 	{
 		assert(first_);
 
-		return T(); // TODO:
+		 // TODO:
+        Node* current = first_;
+        while (current->right)
+            current = current->right;
+        return current->item;
 	}
 
 protected:
