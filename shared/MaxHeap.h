aae08b78170a1ffe9e271e0614a93ea64a3128af
Sung Seunghyun <70056397+moondancer92@users.noreply.github.com>
2025-03-29 19:20:43 +0900

    Solved

--- shared/MaxHeap.h
+++ shared/MaxHeap.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include <cassert>
 #include <iostream>
@@ -52,8 +52,8 @@ public:
 
 	void Push(const T& item)
 	{
-		//using namespace std;
-		//cout << "Push " << item << endl;
+		using namespace std;
+		cout << "Push " << item << endl;
 
 		if (size_ == capacity_)
 			Resize(capacity_ * 2);
@@ -63,15 +63,16 @@ public:
 		size_ += 1;
 		int current = size_; // 마지막에 추가가될 위치 (인덱스)
 
-		while (current != 1 /* && TODO */) // 부모 위치의 값이 추가하려는 값보다 작다면
+        while (current != 1 && heap_[current/2] < item) // 부모 위치의 값이 추가하려는 값보다 작다면
 		{
 			// 부모 위치의 값을 자식 위치로 복사해서 내린다.
 			// TODO:
-
+            T temp = heap_[current/2];
+            heap_[current] = temp;
 			//cout << "Current = " << current << endl;
 			//Print();
-
 			// TODO:
+            current = current/2;
 		}
 
 		heap_[current] = item; // 최종적으로 결정된 위치에 복사
@@ -97,18 +98,23 @@ public:
 		{
 			// left, right 중에서 더 큰 자식의 인덱스를 찾는다. 이때 자식이 하나라면 찾을 필요 없음
 			// TODO:
-
+            if(child < size_ && heap_[child] < heap_[child+1]) child++;
+            
 			// 마지막 값이 더 큰 자식의 값 이상이면 더이상 적절한 위치를 찾을 필요가 없기 때문에 루프 중단
 			// TODO:
+            if(last_item >= heap_[child]) break;
 
 			// 자식 값을 부모 위치로 복사, 
-			// TOD:
-
+			// TODO:
+            heap_[current] = heap_[child];
+            
 			//cout << "Current = " << current << ", child = " << child << endl;
 			//Print();
 
 			// 그 자식 위치로 current 인덱스 변경, child 인덱스도 그 다음 자식 위치로 변경
 			// TODO:
+            current = child;
+            child *= 2;
 		}
 
 		heap_[current] = last_item;
